9b058eecc7462f390da500434a1968eab8ea4c4e
Ruslan Yurchenko <kort9591@gmail.com>
2016-12-22 22:49:11 +0200

    fixed gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 		sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('src/style/sass/**/*.scss')
+  return gulp.src('src/style/sass/main.scss')
     .pipe(sass())
     .pipe(gulp.dest('src/style'));
 });
